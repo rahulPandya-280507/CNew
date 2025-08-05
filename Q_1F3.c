d9aacf50b1a939757db52f68a72e96462cdf85b7
Rahul Pandya <rahulpandya280507@gmail.com>
2025-08-06 03:08:33 +0530

    Q3

---
+++ Q_1F3.c
@@ -0,0 +1,17 @@
+/*Paper of size A0 has dimensions 1189mm x 841mm. Each subsequent size A(n) is defined as A(n-1) cut in half, parallel to it's shorter sides. Thus, paper of size A1 would have dimensions 841mm x 594mm. Write a program to calculate and print paper sizes A0, A1, A2, ..., A8*/
+#include <stdio.h>
+int main(){
+    //this is a simple problem, but you need to know the results
+    /*
+        A0 = 1189*841
+        A1 = 841*594
+        A2 = 594*420
+        A3 = 420*297
+        A4 = 297*210
+        A5 = 210*148
+        A6 = 148*105
+        A7 = 105*74
+        A8 = 74*52
+    */
+   //these are the results. Now you just need to print them. As of now, loops aren't taught, but this can be solved usinf loops and conditional statements.
+}
