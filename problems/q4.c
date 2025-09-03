5600f0dd06bac92c1834a1b3a57de1696864315d
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-03 22:43:52 +0530

    q4

---
+++ problems/q4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n = 25;
+    if(n%2==0)
+    {
+        printf("%d is an even number", n);
+    }
+    else
+    {
+        printf("%d is an odd number", n);
+    }
+    return 0;
+}
