ac439d7ca3e3476a13b340c1d9a3c2dc1bad5096
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-10 11:09:32 +0530

    q12

--- problems/q12.c
+++ problems/q12.c
@@ -1,1 +1,13 @@
 /*Write a C program to check if a person's age is greater than or equal to 18 (eligible to vote) only using if statement.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if(age>=18)
+        printf("You are eligible to vote!");
+    return 0;
+}
