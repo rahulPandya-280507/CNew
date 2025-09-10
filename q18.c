2ca35b7709b1b87324a804a56dba96740053002c
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-10 12:24:17 +0530

    q18

---
+++ q18.c
@@ -0,0 +1,14 @@
+/*Write a C program to get the sum of first n nantural numbers using while loop*/
+#include <stdio.h>
+int main()
+{
+    int n,i=1,sum=0;
+    printf("Enter your limit: ");
+    scanf("%d", &n);
+    while(i<=n)
+    {
+        sum+=i;
+        i++;
+    }
+    printf("%d is the sum of first %d natural numbers!", sum, n);
+}
