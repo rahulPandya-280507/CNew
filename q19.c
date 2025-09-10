2005b7f06892022689677c34974acaf3bada0fff
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-10 12:34:03 +0530

    q19

---
+++ q19.c
@@ -0,0 +1,8 @@
+/*Write a C program to find the factorial using a for loop*/
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number you want to find the factorial of: ");
+    
+}
