923728654a460a6bb2f0309a5c1f89a547a2e6ef
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-10 12:04:05 +0530

    q17_1

---
+++ problems/q17_1.c
@@ -0,0 +1,49 @@
+/*Write a C program to display the months of the year using switch case*/
+#include <stdio.h>
+int main()
+{
+    int month;
+    printf("Which month of the year do you want?: ");
+    scanf("%d", &month);
+    switch (month)
+    {
+    case 1:
+        printf("January");
+        break;
+    case 2:
+        printf("February");
+        break;
+    case 3:
+        printf("March");
+        break;
+    case 4:
+        printf("April");
+        break;
+    case 5:
+        printf("May");
+        break;
+    case 6:
+        printf("June");
+        break;
+    case 7:
+        printf("July");
+        break;
+    case 8:
+        printf("August");
+        break;
+    case 9:
+        printf("September");
+        break;
+    case 10:
+        printf("October");
+        break;
+    case 11:
+        printf("November");
+        break;
+    case 12:
+        printf("December");
+        break;
+    default:
+        break;
+    }
+}
