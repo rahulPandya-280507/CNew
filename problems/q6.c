167fbce93d936573a6e268195ee285298a9f73aa
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-11 00:29:13 +0530

    New update in q6

--- problems/q6.c
+++ problems/q6.c
@@ -7,5 +7,9 @@ int main()
     printf("quotient is: %d\n", a/b);
     printf("product is: %d\n", a*b);
     printf("remainder is: %d\n", a%b);
+    printf("unary plus is: %d\n", +a);
+    printf("unary minus is: %d\n", -a);
+    printf("increment is: %d\n", ++a);
+    printf("decrement is: %d\n", --a);
     return 0;
 }
