a06caaae0ee5406a7ae4a255560ac1d31d561d0b
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-21 21:35:04 +0530

    New updates - minor

--- rank.c
+++ rank.c
@@ -5,6 +5,7 @@ int main() {
     printf("Enter the number of rows and columns of the matrix: ");
     scanf("%d %d", &rows, &columns);
     printf("The order of the matrix is: %d\n", rows*columns);
+
     //declaring and taking input of matrix
     int arr[rows][columns];
     printf("Enter the elements of the matrix row-wise: \n");
@@ -13,6 +14,7 @@ int main() {
             scanf("%d", &arr[i][j]);
         }
     }
+    
     //printing the matrix: 
     printf("The matrix is: \n");
     for(int i=0; i<rows; i++){
