86c196cfa14b126c0acf231108cbdb23bd776771
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-23 22:01:01 +0530

    complete PS2

---
+++ notes and exercises/PS2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    //Q3] write a program to check whether a given number is divisible by 97 or not.
+    /*
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    if(n%97==0)
+        printf("%d is divisible by 97\n", n);
+    else
+        printf("%d is not divisible by 97\n", n);
+    return 0;
+    */
+    
+    
+}
