750be1262be42cbcbcb48c8a58f3f573e26bf06c
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-28 00:07:48 +0530

    u q2->v7

--- patterns/Q2.c
+++ patterns/Q2.c
@@ -107,3 +107,26 @@ int main() {
     }
 }
 */
+
+//floyd's triangle pattern
+/*
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+*/
+/*
+#include <stdio.h>
+int main() {
+    int n, count = 1;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for(int i=0; i<n; i++) {
+        for (int j=0; j<i+1; j++) {
+            printf("%d ", count);
+            count ++;
+        }
+        printf("\n");
+    }
+}
+*/
