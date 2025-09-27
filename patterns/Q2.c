6303fcea9c7fdfcf8b2e77d3559bbaef1de3edaf
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-27 23:49:13 +0530

    u q2->v2

--- patterns/Q2.c
+++ patterns/Q2.c
@@ -1,4 +1,20 @@
+/*
+    *
+    * *
+    * * * 
+    * * * * 
+*/
+/*
 #include <stdio.h>
 int main() {
-    //code goes here
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for(int i=0; i<n; i++) {
+        for(int j = 0; j<=i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
 }
+*/
