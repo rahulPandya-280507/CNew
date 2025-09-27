5ca47beff30a3d9975a6de976519cb51cdeeca90
Rahul Pandya <rahulpandya280507@gmail.com>
2025-09-27 16:14:08 +0530

    New files added

---
+++ patterns/Q1.c
@@ -0,0 +1,32 @@
+//this is a file that teaches you about different patterns you can cretae in C!
+//Q1
+/*
+    1 2 3 4
+    1 2 3 4
+    1 2 3 4
+    1 2 3 4
+*/
+//ALGORITHM
+/*
+    1] Start
+    2] take input of number of rows(i.e. the number of times they want the row to repeat) and the number of columns(i.e. till what number do they want to print the numbers) from the user
+    3] run a loop from 1 to number of rows
+    4] run a nested loop from 1 to number of columns
+    5] end
+*/
+
+    #include <stdio.h>
+    int main() {
+        int rows, columns;
+        printf("Enter the number of times you want the row to repeat: ");
+        scanf("%d", &rows);
+        printf("Enter the number till what number do you want to print the numbers: ");
+        scanf("%d", &columns);
+        for(int i=0; i<rows; i++) {
+            for (int j=0; j<columns; j++) {
+                printf("%d ", j+1);
+            }
+            printf("\n");
+        }
+    }
+
