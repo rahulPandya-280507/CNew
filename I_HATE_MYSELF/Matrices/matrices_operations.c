8424229551f1dac90dba7dafcd2cee362f85d63d
Rahul Pandya <rahulpandya280507@gmail.com>
2025-10-30 22:46:39 +0530

    first local run

--- I_HATE_MYSELF/Matrices/matrices_operations.c
+++ I_HATE_MYSELF/Matrices/matrices_operations.c
@@ -3,21 +3,21 @@
 #include <string.h>
 
 //trying to take the input of a matrix and storing it in a dynamic array 
-// int main() {
-//     int size, *mat;
-//     printf("Enter the size of the 1D array: ");
-//     scanf("%d", &size);
-//     mat = (int*)calloc(size, sizeof(int));
-//     for(int i=0; i<size; i++) {
-//         printf("%d ", mat[i]);
-//     }
-//     printf("\nEnter the elements: \n");
-//     for(int i=0; i<size; i++) {
-//         scanf("%d", &mat[i]);
-//     }
-//     printf("The elements are: \n");
-//     for(int i=0; i<size; i++) {
-//         printf("%d ", mat[i]);
-//     }
-//     free(mat);
-// }
+int main() {
+    int size, *mat;
+    printf("Enter the size of the 1D array: ");
+    scanf("%d", &size);
+    mat = (int*)calloc(size, sizeof(int));
+    for(int i=0; i<size; i++) {
+        printf("%d ", mat[i]);
+    }
+    printf("\nEnter the elements: \n");
+    for(int i=0; i<size; i++) {
+        scanf("%d", &mat[i]);
+    }
+    printf("The elements are: \n");
+    for(int i=0; i<size; i++) {
+        printf("%d ", mat[i]);
+    }
+    free(mat);
+}
