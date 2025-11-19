1735922010e3cb2e11df3791c39e48da8c45e3ab
Rahul Pandya <rahulpandya280507@gmail.com>
2025-11-20 00:14:16 +0530

    some new stuff

--- problems/q30.c
+++ problems/q30.c
@@ -5,9 +5,9 @@ wacp to calculate square root from 1 to 30
 #include <stdio.h>
 #include <math.h>
 int main() {
-    printf("The squares of numbers from 1 to 20 are: \n");
-    for(int i=1; i<=20; i++) {
-        printf("square root of %d = %.2f\n", i, sqrt(i));
+    printf("The square root of numbers from 1 to 30 are: \n");
+    for(int i=1; i<=30; i++) {
+        printf("sqrt(%d) = %.2f\n", i, sqrt(i));
     }
     return 0;
 }
