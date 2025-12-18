fc8e65075262d7c770c4459bd26365b62397dc1e
Rahul Pandya <rahulpandya280507@gmail.com>
2025-12-18 10:48:54 +0530

    Linked List deletion

--- main_folder/linkedList.c
+++ main_folder/linkedList.c
@@ -16,6 +16,10 @@ struct Node* insBeg(struct Node* head, int data);
 struct Node* insIndex(struct Node* head, int data, int index);
 struct Node* insEnd(struct Node* head, int data);
 struct Node* insAfter(struct Node* head, struct Node* prevNode, int data);
+struct Node* delFirst(struct Node* head);
+struct Node* delBet(struct Node* head, int index);
+struct Node* delEnd(struct Node* head);
+struct Node* delGivenValue(struct Node* head, int value);
 
 int main() {
     //creation of nodes
@@ -41,20 +45,31 @@ int main() {
     fourth->data = 10;
     fourth->next=NULL;
 
-    printf("BEFORE INSERTION:\n");
+    printf("BEFORE DELETION:\n");
     linkedListTraversal(head);
 
+
+    //insertion code!!!!
     // int data;
     // printf("Enter the data you want to input into the new node: ");
     // scanf("%d", &data);
-
+    //declaration statements
     // struct Node* newHead = insBeg(head, data);   //inserts a new node at the begining of the LL
     // struct Node* head2 = insIndex(head, data, 2);    //inserts a new node on the 2nd index of the LL
     // struct Node* headEnd = insEnd(head, data);   //Inserts a new node at the end of the LL
     // struct Node* headAfter = insAfter(head, second, data);   //inserts a new node after a given node
 
-    printf("AFTER INSERTION:\n");
-    // linkedListTraversal(headAfter);
+
+
+    //deletion code!!!!
+    // struct Node* delFirstHead = delFirst(head);  //deletes first node of LL
+    // struct Node* delIndexHead = delBet(head, 2); //deletes the node at a given index.
+    // struct Node* delEndHead = delEnd(head);  //deletes the last node of a given LL
+    struct Node* delGVHead = delGivenValue(head, 100);
+
+
+    printf("AFTER DELETION:\n");
+    linkedListTraversal(delGVHead);
     return 0;
 }
 
@@ -106,5 +121,58 @@ struct Node* insAfter(struct Node* head, struct Node* prevNode, int data) {
     ptr->data = data;
     ptr->next = prevNode->next;
     prevNode->next = ptr;
+    return head;
+}
+
+struct Node* delFirst(struct Node* head) {
+    struct Node* ptr = head;
+    head = head->next;
+    free(ptr);
+    return head;
+}
+
+struct Node* delBet(struct Node* head, int index) {
+    struct Node* p = head;
+    int i=0;
+    while(i!=index-1) {
+        p = p->next;
+        i++;
+    }
+    struct Node* q = p->next;
+    p->next = q->next;
+    free(q);
+    return head;
+}
+
+struct Node* delEnd(struct Node* head) {
+    struct Node* p = head;
+    struct Node* q = p->next;
+    while(q->next!=NULL) {
+        p = p->next;
+        q = q->next;
+    }
+    p->next = NULL;
+    free(q);
+    return head;
+}
+
+struct Node* delGivenValue(struct Node* head, int value) {
+    struct Node* p = head;
+    struct Node* q = NULL;
+    if(head!=NULL && head->data == value) {
+        head = head->next;
+        free(p);
+        return head;
+    }
+    while(p!=NULL && p->data!=value) {
+        q = p;
+        p = p->next;
+    }
+    if(p==NULL){
+        printf("Value not found!\n");
+        return head;
+    }
+    q->next = p->next;
+    free(p);
     return head;
 }
