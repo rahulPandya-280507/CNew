c9b25cdf2d1c05033dfc6c304137838ca56bae75
Rahul Pandya <rahulpandya280507@gmail.com>
2026-01-11 13:08:24 +0530

    Queues using linked lists

---
+++ DSA-Folder/queue/queue_ll.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *f = NULL;
+struct Node *r = NULL;
+
+void linkedListTraversal(struct Node *ptr);
+void enqueue(int val);
+int dequeue();
+
+int main()
+{
+    int i = 0;
+    while(i!=10) {
+        enqueue(i);
+        i++;
+    }
+    
+    linkedListTraversal(f);
+    return 0;
+}
+
+void linkedListTraversal(struct Node *ptr)
+{
+    if (ptr == NULL)
+    {
+        printf("The queue is emplty!\n");
+    }
+    else
+    {
+        printf("Printing all the elements of this LL: \n");
+        while (ptr != NULL)
+        {
+            printf("Element: %d\n", ptr->data);
+            ptr = ptr->next;
+        }
+    }
+}
+
+void enqueue(int val)
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    if (n == NULL)
+    {
+        printf("The queue is full!\n");
+    }
+    else
+    {
+        n->data = val;
+        n->next = NULL;
+        if (f == NULL)
+        {
+            f = r = n;
+        }
+        else
+        {
+            r->next = n;
+            r = n;
+        }
+    }
+}
+
+int dequeue()
+{
+    int val = -1;
+    struct Node *ptr = f;
+    if (f == NULL)
+    {
+        printf("The queue is empty!\n");
+    }
+    else
+    {
+        f = f->next;
+        val = ptr->data;
+        free(ptr);
+    }
+    return val;
+}
