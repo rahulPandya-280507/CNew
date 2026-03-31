2cba95f31bc02500971da535cc3459a956b9b557
Rahul Pandya <rahulpandya280507@gmail.com>
2026-03-31 10:39:50 +0530

    initial commit

---
+++ DSA-Folder/tress/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node* createNode(int data);
+
+int main() {
+    struct Node *p;
+    p = (struct Node *)malloc(sizeof(struct Node));
+    p->left = NULL;
+    p->right = NULL;
+    struct Node *p1;
+    p1 = (struct Node *)malloc(sizeof(struct Node));
+    p1->left = NULL;
+    p1->right = NULL;
+    struct Node *p2;
+    p2 = (struct Node *)malloc(sizeof(struct Node));
+    p2->left = NULL;
+    p2->right = NULL;
+    p->left = p1;
+    p->right = p2;
+    return 0;
+}
+
+struct Node* createNode(int data) {
+    struct Node *n;
+    n = (struct Node* )malloc(sizeof(struct Node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+}
