62ca57dd2542edd651a9fe5bba11dd6502baadb6
Rahul Pandya <rahulpandya280507@gmail.com>
2026-04-02 13:06:28 +0530

    src

--- DSA-Folder/tress/main.c
+++ DSA-Folder/tress/main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int data;
     struct Node *left;
     struct Node *right;
 };
 
-struct Node* createNode(int data);
+struct Node *createNode(int data);
+void preOrder(struct Node *root);
+void postOrder(struct Node *root);
+void inOrder(struct Node *root);
 
-int main() {
+int main()
+{
     /*
     struct Node *p;
     p = (struct Node *)malloc(sizeof(struct Node));
@@ -28,18 +33,62 @@ int main() {
     return 0;
     */
     // constructing new node using function
-    struct Node *p = createNode(2);
+    struct Node *p = createNode(4);
     struct Node *p1 = createNode(1);
-    struct Node *p2 = createNode(4);
+    struct Node *p2 = createNode(6);
+    struct Node *p3 = createNode(5);
+    struct Node *p4 = createNode(2);
     p->left = p1;
     p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    // preorder
+    printf("Preorder: \n ");
+    preOrder(p);
+
+    // postorder
+    printf("\nPostorder: \n");
+    postOrder(p);
+
+    // Inorder
+    printf("\nInorder: \n");
+    inOrder(p);
 }
 
-struct Node* createNode(int data) {
+struct Node *createNode(int data)
+{
     struct Node *n;
-    n = (struct Node* )malloc(sizeof(struct Node));
+    n = (struct Node *)malloc(sizeof(struct Node));
     n->data = data;
     n->left = NULL;
     n->right = NULL;
     return n;
+}
+
+void preOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void postOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void inOrder(struct Node *root) {
+    if(root != NULL) {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
 }
